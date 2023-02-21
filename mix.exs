aa1106b6b1f6d4967cdec1b21c176741e69c0e2f
TraceyOnim <traceyonim22@gmail.com>
2023-02-21 11:16:28 +0300

    update version number

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule BlinklyHahaNew.MixProject do
   use Mix.Project
 
   @app :blinkly_haha_new
-  @version "0.1.0"
+  @version "0.2.0"
   @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64, :grisp2]
 
   def project do
